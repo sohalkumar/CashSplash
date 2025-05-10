1b1e2736823664556c8b409e821a9866b8166be0
sohalkumar <sohalkumarsingh@gmail.com>
2025-05-10 11:59:10 +0530

    Added 'Hello world' app through bot

--- CashSplashBot/bot.py
+++ CashSplashBot/bot.py
@@ -1,26 +1,31 @@
 import os
-from dotenv import load_dotenv
 import logging
+from dotenv import load_dotenv
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 # Load environment variables from .env file
 load_dotenv() 
+
 TG_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+WEB_APP_URL = os.getenv('WEBAPP_URL')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    inline_keyboard = [
+        [InlineKeyboardButton("Launch App", web_app=WebAppInfo(url=WEB_APP_URL))]
+    ]
+    reply_markup = InlineKeyboardMarkup(inline_keyboard)
+    await update.message.reply_text(
+        "Click the button below to start playing Cash Splash!",
+        reply_markup=reply_markup
+    )
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(TG_BOT_TOKEN).build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    
-    application.run_polling()
+app = ApplicationBuilder().token(TG_BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.ALL, handle_message))
+app.run_polling()
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,31 @@
 function App() {
   return (
-    <div className="bg-gray-700 text-3xl text-amber-100 text-center p-5">
-      <h1>Hello world</h1>
+    <div className="">
+      <h1 className="bg-gray-700 text-3xl text-amber-100 text-center p-5">Hello world</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque magni
+        dolores nemo dignissimos ut facere rem adipisci similique cupiditate
+        temporibus. Deleniti minus inventore repellendus, reprehenderit ut ipsam
+        ducimus quaerat accusamus corrupti unde repudiandae quos rem eum nemo
+        labore quis aut quibusdam, voluptatibus cum enim numquam velit,
+        dignissimos officia cupiditate. Sit nisi odit atque tempore qui
+        dignissimos harum cumque deleniti ex eius vitae exercitationem a
+        voluptates porro nam perspiciatis asperiores, animi quod sequi aliquam!
+        Nam ex inventore modi at quos, cumque repellendus sit temporibus quam
+        iusto quae labore nobis natus sunt accusamus consequatur? Quos
+        praesentium at esse eum quisquam animi veniam cumque dolores? Deserunt
+        commodi ipsum animi ea eaque, fugit tempora quia molestiae aliquid,
+        necessitatibus quam iusto iure culpa deleniti corrupti. Error nostrum
+        veniam commodi voluptate illo iste et aperiam recusandae aspernatur,
+        repellat saepe eligendi! Non nulla quis quae officia ipsum distinctio
+        commodi iste! Quia molestiae ut reprehenderit nobis necessitatibus
+        similique architecto consequatur numquam quod rerum. Amet illum debitis
+        velit cupiditate ex, maiores ipsa enim adipisci temporibus, iste officia
+        suscipit provident a nisi voluptates perspiciatis nobis at molestias
+        rerum vel laudantium autem! Assumenda pariatur officiis ducimus esse!
+        Recusandae, cum eaque laudantium tempora nemo ut blanditiis aperiam
+        possimus est exercitationem consequuntur doloribus.
+      </div>
     </div>
   );
 }
